a98d6e58043dbce203c9a0ef6680b103c3c6645c
CypressG <krasauskas.kipras@gmail.com>
2021-06-08 18:10:46 +0300

    Prepared model fields

--- testio/tests/models.py
+++ testio/tests/models.py
@@ -2,16 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Tests(models.Model):
-    type = models.BooleanField()
-    pass
+    type = models.CharField()
+    fk_user = models.ForeignKey()
 
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
     question = models.CharField()
-    category = models.ForeignKey()
+    #Tag can be also ManyToMany need to look up
+    tags = models.ForeignKey() 
     explanation = models.CharField()
-    type = models.BooleanField()
-    pass
+    fk_tests = models.ForeignKey()
+
+class Answer(models.Model):
+    id = models.AutoField(primary_key=True)
+    fk_question = models.ForeignKey()
+    right_answer = models.BooleanField()
+    answer = models.CharField()
+
+
 
 class Categories(models.Model):
     pass
