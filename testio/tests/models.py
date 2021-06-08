e8fee9f4809da7ddb050d56e49a41bfb01016a10
Laurynas Gudavičius <s037845@ad.viko.lt>
2021-06-08 18:47:09 +0300

    something was changed

--- testio/tests/models.py
+++ testio/tests/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
 class Tests(models.Model):
     type = models.CharField()
-    fk_user = models.ForeignKey()
+    #fk_user = models.ForeignKey()
 
 class Question(models.Model):
     id = models.AutoField(primary_key=True)
@@ -11,15 +13,22 @@ class Question(models.Model):
     #Tag can be also ManyToMany need to look up
     tags = models.ForeignKey() 
     explanation = models.CharField()
+    #right_answer = models.CharField()
     fk_tests = models.ForeignKey()
 
 class Answer(models.Model):
     id = models.AutoField(primary_key=True)
     fk_question = models.ForeignKey()
-    right_answer = models.BooleanField()
+    #right_answer = models.BooleanField()
+    right_answer = models.CharField()
     answer = models.CharField()
 
+class Tags(models.Model):
+    id = models.AutoField(primary_key=True)
+    tag = models.CharField()
 
-
-class Categories(models.Model):
-    pass
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    comment = models.CharField()
+    fk_tests = models.ForeignKey(Tests)
+    #fk_user = models.ForeignKey(User)
