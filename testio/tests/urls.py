0452d719065953461e0f5be869fef3db2be5fe81
CypressG <krasauskas.kipras@gmail.com>
2021-06-09 10:33:43 +0300

    fixed merge conflicts

--- testio/tests/urls.py
+++ testio/tests/urls.py
@@ -4,9 +4,6 @@ from . import views
 urlpatterns = [
     path('tags/', views.tags,name='tags'),
     path('tests/',views.tests,name='tests'),
-<<<<<<< HEAD
-=======
     path('question/',views.questions,name='question'),
     path('answer/',views.answer,name='answer'),
->>>>>>> 651841937da993e74f57b1fdca041f049dff99d2
 ]
